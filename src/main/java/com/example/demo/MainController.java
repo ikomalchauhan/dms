edf3384c450e15c7be5376870ac14d052d565679
Terx Labs <terxlabs@gmail.com>
2022-06-01 18:06:07 +0530

    changes in api

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -9,16 +9,23 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
 
     @PostMapping("/api")
-    public ResponseEntity<String> postAPI(@RequestHeader("X-ADOBESIGN-CLIENTID") String clientId) {
+    public ResponseEntity<ResponseObj> postAPI(@RequestHeader("X-ADOBESIGN-CLIENTID") String clientId) {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("X-AdobeSign-ClientId", clientId);
-        return ResponseEntity.ok().headers(responseHeaders).body("POST API is working");
+        ResponseObj obj = new ResponseObj();
+        obj.setRequestType("POST API is working");
+        obj.setxAdobeSignClientId(clientId);
+        return ResponseEntity.ok().headers(responseHeaders).body(obj);
     }
 
     @GetMapping("/api")
-    public ResponseEntity<String> getAPI(@RequestHeader("X-ADOBESIGN-CLIENTID") String clientId) {
+    public ResponseEntity<ResponseObj> getAPI(@RequestHeader("X-ADOBESIGN-CLIENTID") String clientId) {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("X-AdobeSign-ClientId", clientId);
-        return ResponseEntity.ok().headers(responseHeaders).body("GET API is working");
+        ResponseObj obj = new ResponseObj();
+        obj.setRequestType("GET API is working");
+        obj.setxAdobeSignClientId(clientId);
+        return ResponseEntity.ok().headers(responseHeaders).body(obj);
     }
 }
+
---
+++ src/main/java/com/example/demo/ResponseObj.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class ResponseObj{
+    private String requestType;
+    private String xAdobeSignClientId;
+
+    public String getRequestType() {
+        return requestType;
+    }
+
+    public void setRequestType(String requestType) {
+        this.requestType = requestType;
+    }
+
+    public String getxAdobeSignClientId() {
+        return xAdobeSignClientId;
+    }
+
+    public void setxAdobeSignClientId(String xAdobeSignClientId) {
+        this.xAdobeSignClientId = xAdobeSignClientId;
+    }
+}
