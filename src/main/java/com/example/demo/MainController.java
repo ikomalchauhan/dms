afe05863c27eaedfd6bb73394cfd1358b8c40311
Komal Chauhan <76562738+ikomalchauhan@users.noreply.github.com>
2022-06-06 18:39:13 +0530

    Update MainController.java

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -9,24 +9,26 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
 
     @PostMapping("/api")
-    public ResponseEntity<ResponseObj> postAPI(@RequestHeader("X-ADOBESIGN-CLIENTID") String clientId) {
+    public ResponseEntity<ResponseObj> postAPI(@RequestHeader("X-ADOBESIGN-CLIENTID") String clientId, @RequestBody Map<String, Object>[] payload) {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("X-AdobeSign-ClientId", clientId);
         ResponseObj obj = new ResponseObj();
         obj.setRequestType("POST API is working");
         obj.setxAdobeSignClientId(clientId);
         System.out.println("POST API is working");
+        System.out.println(payload);
         return ResponseEntity.ok().headers(responseHeaders).body(obj);
     }
 
     @GetMapping("/api")
-    public ResponseEntity<ResponseObj> getAPI(@RequestHeader("X-ADOBESIGN-CLIENTID") String clientId) {
+    public ResponseEntity<ResponseObj> getAPI(@RequestHeader("X-ADOBESIGN-CLIENTID") String clientId, @RequestBody Map<String, Object>[] payload) {
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set("X-AdobeSign-ClientId", clientId);
         ResponseObj obj = new ResponseObj();
         obj.setRequestType("GET API is working");
         obj.setxAdobeSignClientId(clientId);
         System.out.println("GET API is working");
+        System.out.println(payload);
         return ResponseEntity.ok().headers(responseHeaders).body(obj);
     }
 }
