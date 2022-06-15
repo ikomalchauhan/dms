98e96b13499a12cb7f6ea37cf7672ea0e247ad53
Komal Chauhan <76562738+ikomalchauhan@users.noreply.github.com>
2022-06-15 15:10:36 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/Agreement.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+public class Agreement {
+    private String id;
+    private String name;
+    private String status;
+
+    public Agreement(String id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Agreement{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/example/demo/DataDTO.java
+++ src/main/java/com/example/demo/DataDTO.java
@@ -7,6 +7,7 @@ public class DataDTO {
     private String webhookName;
     private String webhookNotificationId;
     private String webhookScope;
+    private Agreement agreement;
 
     public void setWebhookId(String webhookId) {
         this.webhookId = webhookId;
@@ -24,6 +25,30 @@ public class DataDTO {
         this.webhookScope = webhookScope;
     }
 
+    public String getWebhookId() {
+        return webhookId;
+    }
+
+    public String getWebhookName() {
+        return webhookName;
+    }
+
+    public String getWebhookNotificationId() {
+        return webhookNotificationId;
+    }
+
+    public String getWebhookScope() {
+        return webhookScope;
+    }
+
+    public Agreement getAgreement() {
+        return agreement;
+    }
+
+    public void setAgreement(Agreement agreement) {
+        this.agreement = agreement;
+    }
+
     @Override
     public String toString() {
         return "DataDTO{" +
@@ -31,6 +56,7 @@ public class DataDTO {
                 ", webhookName='" + webhookName + '\'' +
                 ", webhookNotificationId='" + webhookNotificationId + '\'' +
                 ", webhookScope='" + webhookScope + '\'' +
+                ", agreement=" + agreement +
                 '}';
     }
 }
