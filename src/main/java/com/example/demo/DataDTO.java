a5ec6f313fa3104e6129a3f29d5919de5a132a56
Komal Chauhan <76562738+ikomalchauhan@users.noreply.github.com>
2022-06-15 15:58:28 +0530

    Add files via upload

--- src/main/java/com/example/demo/DataDTO.java
+++ src/main/java/com/example/demo/DataDTO.java
@@ -7,6 +7,13 @@ public class DataDTO {
     private String webhookName;
     private String webhookNotificationId;
     private String webhookScope;
+    private String event;
+    private String eventDate;
+    private String eventResourceType;
+    private String participantRole;
+    private String actionType;
+    private String participantUserId;
+    private String participantUserEmail;
     private Agreement agreement;
 
     public void setWebhookId(String webhookId) {
@@ -49,6 +56,62 @@ public class DataDTO {
         this.agreement = agreement;
     }
 
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getEventResourceType() {
+        return eventResourceType;
+    }
+
+    public void setEventResourceType(String eventResourceType) {
+        this.eventResourceType = eventResourceType;
+    }
+
+    public String getParticipantRole() {
+        return participantRole;
+    }
+
+    public void setParticipantRole(String participantRole) {
+        this.participantRole = participantRole;
+    }
+
+    public String getActionType() {
+        return actionType;
+    }
+
+    public void setActionType(String actionType) {
+        this.actionType = actionType;
+    }
+
+    public String getParticipantUserId() {
+        return participantUserId;
+    }
+
+    public void setParticipantUserId(String participantUserId) {
+        this.participantUserId = participantUserId;
+    }
+
+    public String getParticipantUserEmail() {
+        return participantUserEmail;
+    }
+
+    public void setParticipantUserEmail(String participantUserEmail) {
+        this.participantUserEmail = participantUserEmail;
+    }
+
     @Override
     public String toString() {
         return "DataDTO{" +
@@ -56,6 +119,13 @@ public class DataDTO {
                 ", webhookName='" + webhookName + '\'' +
                 ", webhookNotificationId='" + webhookNotificationId + '\'' +
                 ", webhookScope='" + webhookScope + '\'' +
+                ", event='" + event + '\'' +
+                ", eventDate='" + eventDate + '\'' +
+                ", eventResourceType='" + eventResourceType + '\'' +
+                ", participantRole='" + participantRole + '\'' +
+                ", actionType='" + actionType + '\'' +
+                ", participantUserId='" + participantUserId + '\'' +
+                ", participantUserEmail='" + participantUserEmail + '\'' +
                 ", agreement=" + agreement +
                 '}';
     }
